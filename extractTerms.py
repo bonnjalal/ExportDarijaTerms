f09e4abca9d7aade4361be8ccc1e8b33eca41534
bonnjalal <bonnjalal@gmail.com>
2023-08-19 20:44:45 +0100

    improve code

--- extractTerms.py
+++ extractTerms.py
@@ -4,6 +4,8 @@ import requests
 import json
 
 
+site = pywikibot.Site("en", "wiktionary")
+#user = pywikibot.User(site, "Bonnjalal00")
 def getSecInfo(secDict, line):
     for x in secDict["parse"]["sections"]:
         if x["line"] == line:
@@ -20,36 +22,95 @@ def getAllSect(secDict, startNum):
             sections[num] = x["index"]
     return sections
 
+def isArticle(page):
+    # print(page.namespace().normalize_name())
+    if str(page.namespace()) == ":":
+        return True
+    return False
+    # names = ["Module","Template","User","Wiktionary","File","MediaWiki","Thread","Help","Summary","Appendix",
+    #          "Concordance","Rhymes","Transwiki","Thesaurus","Citations","Sign gloss","Reconstruction", ]
+    # for name in names:
+    #     if title.startswith(name+":"):
+    #         return false
+    # return true
 
-site = pywikibot.Site("en", "wiktionary")
+
+
+
+def getArticlesData(pagesList):
+    for page in pagesList:
+        title = page.title()
+        if isArticle(page):
+            print(title)
+            sectionsJson = requests.get("https://en.wiktionary.org/w/api.php?action=parse&prop=sections&page={}&format=json".format(title))
+            sectionsDict = sectionsJson.json()
+            secIndex, secNum = getSecInfo(sectionsDict,"Moroccan Arabic") 
+            allSections = getAllSect(sectionsDict, secNum)
+
+            for num, index in allSections.items():
+                print(num,index)
+                selectedSectJson = requests.get("https://en.wiktionary.org/w/api.php?action=parse&prop=wikitext&section={}&page={}&format=json".format(index,title))
+                sectionText = selectedSectJson.json()
+                print(sectionText["parse"]["wikitext"])
+                print(num.count("."))
+
+
+def extractCatName(cat):
+    title = cat
+    return title.replace("Category:", "")
+
+def main (catName = "Moroccan Arabic language"):
+    cat = pywikibot.Category(site, catName)
+    catInfo = cat.categoryinfo
+    pagesCount = catInfo["pages"]
+    subcatsCount = catInfo["subcats"]
+    print(pagesCount)
+    if  pagesCount >= 1:
+        getArticlesData(list(cat.articles()))
+
+    if subcatsCount >=1:
+        catList = list(cat.subcategories())
+        for category in catList:
+            name = extractCatName(category.title())
+            #main(name)
+
+main()
+
+
+
+
+# site = pywikibot.Site("en", "wiktio nary")
 #user = pywikibot.User(site, "Bonnjalal00")
 #cat = pywikibot.Category(site, "Moroccan Arabic templates")
-cat = pywikibot.Category(site, "Moroccan Arabic language")
+# cat = pywikibot.Category(site, "Moroccan Arabic language")
 #mem = cat.members()
 
 #for user in range(10):
 #    print (list(allusers).get(user))
 
-catInfo = cat.categoryinfo
-
-pagesCount = catInfo["pages"]
-subcatsCount = catInfo["subcats"]
+# catInfo = cat.categoryinfo
+#
+# pagesCount = catInfo["pages"]
+# subcatsCount = catInfo["subcats"]
 
 
 #print(type(pagesCount))
 #print(subcatsCount)
 
-print(cat.categoryinfo)
-catList = list(cat.subcategories())
+# print(pagesCount)
+# if pagesCount >= 1:
+#     getArticlesData(list(cat.articles()))
+#
+# catList = list(cat.subcategories())
 #print(catList)
-title = list(catList[0].articles())[7].title()
-print(title)
-
-sectionsJson = requests.get("https://en.wiktionary.org/w/api.php?action=parse&prop=sections&page={}&format=json".format(title))
-sectionsDict = sectionsJson.json()
-secIndex, secNum = getSecInfo(sectionsDict,"Moroccan Arabic") 
-
-allSections = getAllSect(sectionsDict, secNum)
+#title = list(catList[0].articles())[0].title()
+# print(title)
+#
+# sectionsJson = requests.get("https://en.wiktionary.org/w/api.php?action=parse&prop=sections&page={}&format=json".format(title))
+# sectionsDict = sectionsJson.json()
+# secIndex, secNum = getSecInfo(sectionsDict,"Moroccan Arabic") 
+#
+# allSections = getAllSect(sectionsDict, secNum)
 #selectedSectJson = requests.get("https://en.wiktionary.org/w/api.php?action=parse&prop=wikitext&section={}&page={}&format=json".format(secIndex,title))
 #sectionText = selectedSectJson.json()
 
@@ -59,14 +120,14 @@ allSections = getAllSect(sectionsDict, secNum)
 # text = list(catList[0].articles())[0].text
 # print(sectionsDict["parse"]["sections"][17])
 
-for num, index in allSections.items():
-    print(num,index)
-    selectedSectJson = requests.get("https://en.wiktionary.org/w/api.php?action=parse&prop=wikitext&section={}&page={}&format=json".format(index,title))
-    sectionText = selectedSectJson.json()
-    print(sectionText["parse"]["wikitext"])
-    print(num.count("."))
-print(allSections)
-
+# for num, index in allSections.items():
+#     print(num,index)
+#     selectedSectJson = requests.get("https://en.wiktionary.org/w/api.php?action=parse&prop=wikitext&section={}&page={}&format=json".format(index,title))
+#     sectionText = selectedSectJson.json()
+#     print(sectionText["parse"]["wikitext"])
+#     print(num.count("."))
+# print(allSections)
+#
 
 ## convert json to xlxs (needs to convert dict to json first)
 # import  jpype     
